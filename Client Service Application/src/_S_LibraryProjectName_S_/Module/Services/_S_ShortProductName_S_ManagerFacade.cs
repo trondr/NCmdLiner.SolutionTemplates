8e0ed1f8c571c0553f2df1aafd9b5035ca5fd8cd
trondr <trondr@outlook.com>
2015-12-20 10:49:07 +0100

    Added service script installer

--- Client Service Application/src/_S_LibraryProjectName_S_/Module/Services/_S_ShortProductName_S_ManagerFacade.cs
+++ Client Service Application/src/_S_LibraryProjectName_S_/Module/Services/_S_ShortProductName_S_ManagerFacade.cs
@@ -3,9 +3,7 @@ using _S_LibraryProjectName_S_._S_ShortProductName_S_Service;
 
 
 namespace _S_LibraryProjectName_S_.Module.Services
-{
-    //_S_ShortProductName_S_ManagerFacade
-
+{    
     public class _S_ShortProductName_S_ManagerFacade : I_S_ShortProductName_S_ManagerFacade
     {
         public void SomeExampleServiceMethod(string someExampleparameter)
