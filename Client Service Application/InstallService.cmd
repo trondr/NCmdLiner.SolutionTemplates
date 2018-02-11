a09564030688b984b28bd335fcce4bcfe413b82a
trondr <trondr@outlook.com>
2018-02-11 02:22:03 +0100

    Code cleanup

--- Client Service Application/InstallService.cmd
+++ Client Service Application/InstallService.cmd
@@ -1,5 +1,5 @@
 @ECHO OFF
 @Echo Installing service
-InstallUtil "%~dp0bin\Release\_S_ServiceProjectName_S_\_S_ServiceProjectName_S_.exe"
+"%~dp0bin\Release\_S_ServiceProjectName_S_\_S_ServiceProjectName_S_.exe" install
 sc start "_S_ShortProductName_S_Service"
 
--- Client Service Application/UnInstallService.cmd
+++ Client Service Application/UnInstallService.cmd
@@ -1,5 +1,4 @@
 @ECHO OFF
-@Echo Installing service
-sc stop "_S_ShortProductName_S_Service"
-InstallUtil /uninstall "%~dp0bin\Release\_S_ServiceProjectName_S_\_S_ServiceProjectName_S_.exe"
+@Echo Uninstalling service
+"%~dp0bin\Release\_S_ServiceProjectName_S_\_S_ServiceProjectName_S_.exe" uninstall
 
